43424044a3b24ffb1aaa34b2cdd22c3ba13a09b5
Atharva7057 <atharvapatil7057@gmail.com>
2025-01-12 18:05:00 +0530

    testing commit from gitbash

--- Backend/PGMates/src/main/java/com/pgmates/controller/UserController.java
+++ Backend/PGMates/src/main/java/com/pgmates/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.pgmates.controller;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.pgmates.dto.LoginRequest;
 import com.pgmates.dto.PropertyDetailsDto;
 import com.pgmates.dto.PropertyDto;
 import com.pgmates.dto.ReviewInfo;
 import com.pgmates.dto.UserDto;
 import com.pgmates.service.UserServices;
-
 import jakarta.validation.Valid;
 
 @RestController
