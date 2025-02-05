43156fb562d4b559513a28bc0aa0fbec0c00cdfc
Ayushi Yadav <ayushi.y0210@gmail.com>
2025-02-05 18:25:41 +0530

    Admin Api Completed

--- Backend/PGMates/src/main/java/com/pgmates/dao/UserDao.java
+++ Backend/PGMates/src/main/java/com/pgmates/dao/UserDao.java
@@ -1,13 +1,18 @@
 package com.pgmates.dao;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pgmates.entity.User;
+import com.pgmates.enums.Role;
 
 public interface UserDao extends JpaRepository<User, Integer> {
 	boolean existsByEmail(String email);
 	Optional <User>  findByEmailAndPassword(String email,String password);
+	
+	List<User> findByRole(Role role);
+	int countByRole(Role role);
 
 }
